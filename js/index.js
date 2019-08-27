f7d81f5018a7a4fe9fd4862d83f6d2195eb43fdd
zzallange <52703429+zzallange@users.noreply.github.com>
2019-08-27 16:56:09 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
     
+    //header_search버튼
+    $('.btn_search').click(function(){
+        var state = $('.in_sear').css('display');
+        if(state == 'none'){
+            $(this).attr('class','btn_searchon')
+            $('.in_sear').show();
+        }else{
+            $(this).attr('class','btn_search')
+            $('.in_sear').hide();
+        }
+    });
+    
     //프리미엄 방산 기업 fadeInOut
     $('.company').slick({
         dots: true,
